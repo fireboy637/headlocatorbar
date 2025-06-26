956712cab368ed5ac088c5d2dc1c67a97ad2225c
mcrcortex <18544518+MCRcortex@users.noreply.github.com>
2025-06-26 23:34:32 +1000

    scale thingie

--- src/main/java/me/cortex/headlocatorbar/Headlocatorbar.java
+++ src/main/java/me/cortex/headlocatorbar/Headlocatorbar.java
@@ -5,11 +5,4 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
 
 public class Headlocatorbar {
-    public static void doThing(GuiGraphics instance, ResourceLocation resourceLocation, int i, int j, int k, int l, int m) {
-        float U0 = 1f/8;
-        float V0 = 1f/8;
-        float U1 = 2f/8;
-        float V1 = 2f/8;
-        instance.blit(resourceLocation, i, j, i+k, j+ l, U0,U1,V0,V1);
-    }
 }
--- src/main/java/me/cortex/headlocatorbar/mixin/MixinLocatorBarRenderer.java
+++ src/main/java/me/cortex/headlocatorbar/mixin/MixinLocatorBarRenderer.java
@@ -5,11 +5,16 @@ import me.cortex.headlocatorbar.Headlocatorbar;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.contextualbar.LocatorBarRenderer;
+import net.minecraft.client.resources.WaypointStyle;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.waypoints.TrackedWaypoint;
+import net.minecraft.world.waypoints.Waypoint;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,18 +23,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LocatorBarRenderer.class)
 public class MixinLocatorBarRenderer {
+    @Shadow @Final private Minecraft minecraft;
     @Unique private ResourceLocation blitOverride;
+    @Unique private TrackedWaypoint waypoint;
 
     @Inject(method = "method_70870", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIIII)V",shift = At.Shift.BEFORE))
     private void injectRender(Level level, GuiGraphics guiGraphics, int i, TrackedWaypoint trackedWaypoint, CallbackInfo ci) {
-        var lvl = Minecraft.getInstance().level;
-        if (lvl != null && trackedWaypoint.id().left().isPresent()) {
-            var entity = lvl.getEntity(trackedWaypoint.id().left().get());
-            if (entity instanceof Player player) {
-                this.blitOverride = Minecraft.getInstance().getSkinManager().getInsecureSkin(player.getGameProfile()).texture();
+        this.waypoint = trackedWaypoint;
+        var connection = Minecraft.getInstance().getConnection();
+        if (connection != null && trackedWaypoint.id().left().isPresent()) {
+            var info = connection.getPlayerInfo(trackedWaypoint.id().left().get());
+            if (info != null) {
+                this.blitOverride = Minecraft.getInstance().getSkinManager().getInsecureSkin(info.getProfile()).texture();
             }
         }
-        trackedWaypoint.id();
     }
 
     @Redirect(method = "method_70870", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIIII)V"))
@@ -37,7 +44,16 @@ public class MixinLocatorBarRenderer {
         if (this.blitOverride == null) {
             instance.blitSprite(renderPipeline, resourceLocation, i, j, k, l, m);
         } else {
-            Headlocatorbar.doThing(instance, this.blitOverride, i, j, k, l,m);
+            float distance = Mth.sqrt((float)this.waypoint.distanceSquared(this.minecraft.cameraEntity));
+            Waypoint.Icon icon = this.waypoint.icon();
+            WaypointStyle style = this.minecraft.getWaypointStyles().get(icon.style);
+            float progress = 1-Mth.clamp((distance-style.nearDistance())/(style.farDistance()-style.nearDistance()),0,1);
+            k = Mth.lerpInt(progress, 4*100, (k+1)*100);
+            l = Mth.lerpInt(progress, 4*100, (l+1)*100);
+            instance.pose().pushMatrix();
+            instance.pose().scale(0.01f);
+            instance.blit(this.blitOverride, (i*100+450)-(k/2), (j*100+450)-(l/2), (i*100+450)-(k/2)+k, (j*100+450)-(l/2)+l, 1f/8,2f/8,1f/8,2f/8);
+            instance.pose().popMatrix();
         }
         this.blitOverride = null;
     }
